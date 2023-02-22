915fa6fbfa9400a7f7724a73aed4d436dd2c2ffa
bhuttobhutto836 <118780115+bhuttobhutto836@users.noreply.github.com>
2023-02-22 13:58:53 -0500

    Initial Commit

---
+++ server.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => res.send('API works'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
