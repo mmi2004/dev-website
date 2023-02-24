6c0fa2a103996baeaefbf3ff5257f7e8d4134a96
Muhammad Mujtaba Iqbal <73756434+mmi2004@users.noreply.github.com>
2023-02-23 19:23:29 -0500

    finalizing routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,6 +1,35 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+const Post = require('../../models/Post');
+const { check, validationResult } = require('express-validator');
 
-router.get('/', (req, res) => res.send('Posts Route'));
+// create a post
+router.post('/', [ auth, [check('text', 'text required').not().isEmpty()] ], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        const newPost = new Post ({
+            text: req.body.text,
+            name: user.name,
+            avatar: user.avatar,
+            user: req.user.id
+        });
+        const post = await newPost.save();
+        res.json(post);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error')
+    }
+});
+
+// get all posts
+router.get('/', auth)
+
 
 module.exports = router;
